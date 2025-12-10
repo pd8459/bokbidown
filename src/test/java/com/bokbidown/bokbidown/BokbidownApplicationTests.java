f88f36f27cadbfc127741e2a33e098ca1ec165ac
RestfulApiBoard <pd8459@gmail.com>
2025-12-10 15:53:57 +0900

    Init project

---
+++ src/test/java/com/bokbidown/bokbidown/BokbidownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bokbidown.bokbidown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokbidownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
